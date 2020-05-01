eb22ef059bf90d38d535e597a1530224958e72d0
Owen Smith <owen@omsmith.ca>
2020-05-01 16:32:12 -0400

    test: merge coverage results with nyc (#147)

--- bin/test-all.sh
+++ bin/test-all.sh
@@ -6,6 +6,8 @@ source "$(dirname ${BASH_SOURCE[0]})/helpers.sh"
 
 TOP_DIR=$(pwd)
 
+rm -r .nyc_output 2>/dev/null || :
+
 for package in $(get_packages); do
 	cd "${package}"
 	npm test
@@ -13,9 +15,13 @@ for package in $(get_packages); do
 	cd "${TOP_DIR}"
 	if [ -d "${package}/.nyc_output" ]; then
 		./node_modules/.bin/nyc \
-			report \
-			--temp-directory "${package}/.nyc_output" \
-			--reporter lcov \
-			--report-dir "coverage/$(basename "${package}")"
+			merge \
+			"${package}/.nyc_output" \
+			".nyc_output/$(basename "${package}").json"
 	fi
 done
+
+./node_modules/.bin/nyc report \
+	--exclude-node-modules false \
+	--reporter text \
+	--reporter lcov
